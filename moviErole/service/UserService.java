97907c14159a086666161eecf592947efa8334a5
artachojf <100614773+artachojf@users.noreply.github.com>
2022-04-09 17:59:32 +0200

    Delete moviErole/service directory

--- moviErole/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.erole.moviErole.service;
-
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-import com.erole.moviErole.model.User;
-
-public interface UserService extends UserDetailsService{
-	User save(User form);
-}
