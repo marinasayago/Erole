8e1b863408eac3ddc444851b86cb67a137abdfa9
ivandelgadoalba@uma.es <ivandelgadoalba@uma.es>
2022-05-24 11:22:11 +0200

    Corregido error en las clases Test

--- moviErole/src/test/java/com/erole/moviErole/ActorTest.java
+++ moviErole/src/test/java/com/erole/moviErole/ActorTest.java
@@ -6,18 +6,10 @@ import org.junit.jupiter.api.Test;
 
 import static org.mockito.Mockito.*;
 
-import java.util.Collection;
-import java.util.Date;
-import java.util.LinkedList;
-
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
 import com.erole.moviErole.APIQuery.model.contentQuery.Actor;
-import com.erole.moviErole.model.Role;
-import com.erole.moviErole.model.User;
 
 class ActorTest {
 
@@ -38,7 +30,7 @@ class ActorTest {
 		assertEquals(actor.getName(), null);
 	}
 
-	@Test //Tras insertar datos, el usuario contiene información
+	@Test //Tras insertar datos, el actor contiene información
 	void contieneInformacion() {
 		actor = new Actor("id", "image", "name", "asCharacter");
 		/*public User(String email, String userName, String password, String name, String surname, Date birthdate,
@@ -51,16 +43,41 @@ class ActorTest {
 	
 	@Test //Tras cambiar los atributos a unos diferentes, debería ser distintos al actor original
 	void setTest() {
-		actor = new Actor("id", "image", "name", "asCharacter");
-		Actor auxActor = actor;
-		auxActor.setId("id2");
-		auxActor.setImage("image2");
-		auxActor.setName("name2");
-		auxActor.setAsCharacter("asCharacter2");
-		assertEquals(false, actor.getId().compareTo(auxActor.getId()) != 0);
-		assertEquals(false, actor.getImage().compareTo(auxActor.getImage()) != 0);
-		assertEquals(false, actor.getName().compareTo(auxActor.getName()) != 0);
-		assertEquals(false, actor.getAsCharacter().compareTo(auxActor.getAsCharacter()) != 0);
+		Actor actor1 = mock(Actor.class); //Creamos los mock de dos objetos Actor
+		Actor actor2 = mock(Actor.class);
+			
+		String id1 = "1";
+		String id2 = "2";
+		
+		when(actor1.getId()).thenReturn(id1); //Asignamos los id de los actores
+		when(actor2.getId()).thenReturn(id2);
+		actor1.setId(id2); //Cambiamos el id del primer actor
+			
+		String img1 = "1";
+		String img2 = "2";
+			
+		when(actor1.getImage()).thenReturn(img1); //Asignamos las imagenes
+		when(actor2.getImage()).thenReturn(img2);
+		actor1.setImage(img2); //Cambiamos la imagen del primer actor
+			
+		String name1 = "1";
+		String name2 = "2";
+			
+		when(actor1.getName()).thenReturn(name1); //Asignamos los nombres
+		when(actor2.getName()).thenReturn(name2);
+		actor1.setName(name2); //Cambiamos el nombre del primer actor
+			
+		String char1 = "1";
+		String char2 = "2";
+			
+		when(actor1.getAsCharacter()).thenReturn(char1); //Asigna los papeles
+		when(actor2.getAsCharacter()).thenReturn(char2);
+		actor1.setAsCharacter(char2); //Cambia el papel del primer actor
+			
+		assertEquals(false, actor1.getId().equals(actor2.getId())); //Comprueba el id
+		assertEquals(false, actor1.getImage().equals(actor2.getImage())); //Comprueba la imagen
+		assertEquals(false, actor1.getName().equals(actor2.getName())); //Comprueba el nombre
+		assertEquals(false, actor1.getAsCharacter().equals(actor2.getAsCharacter())); //Comprueba el personaje
 	}
 	
 }
--- moviErole/src/test/java/com/erole/moviErole/UserTest.java
+++ moviErole/src/test/java/com/erole/moviErole/UserTest.java
@@ -47,9 +47,9 @@ class UserTest {
 		roles = new LinkedList<Role>();
 		roles.add(rol);
 		user = new User("email", "userName", "password", "name", "surname", date,
-				"bio", roles, "", "");
-		/*public User(String email, String userName, String password, String name, String surname, Date birthdate,
-				String bio, Collection<Role> roles)*/
+				"bio", roles, "watchLater", "myMovies");
+		/*String email, String userName, String password, String name, String surname, Date birthdate,
+			String bio, Collection<Role> roles, String watchLater, String myMovies*/
 		assertEquals(false, user.getEmail().isEmpty());
 		assertEquals(false, user.getName().isEmpty());
 		assertEquals(false, user.getPassword().isEmpty());
@@ -58,41 +58,113 @@ class UserTest {
 		assertEquals(false, user.getBirthdate().equals(null));
 		assertEquals(false, user.getBio().isEmpty());
 		assertEquals(false, user.getRoles().isEmpty());
-	}
+		assertEquals(false, user.getMyMovies().isEmpty());
+		assertEquals(false, user.getWatchLater().isEmpty());
+	} 
+
 	
-	@Test //Tras cambiar los atributos a unos diferentes, debería ser distintos al usuario original
-	void setTest() {
+	public void setTest(){
+		User user1 = mock(User.class); //Creamos mock de dos objetos usuario
+		User user2 = mock(User.class);
+		
+		Integer id1 = 0;
+		Integer id2 = 1;
+		
+		when(user1.getId()).thenReturn(id1); //Asignamos un identificador
+		when(user2.getId()).thenReturn(id2); 
+		user1.setId(id2); //Cambiamos el identificador del primer usuario al del segundo
+		
+		Collection<Role> roles1;
+		Role rol1 = new Role("Rol1");
+		roles1 = new LinkedList<Role>();
+		roles1.add(rol1);
+		
+		Collection<Role> roles2;
+		Role rol2 = new Role("Rol2");
+		roles2 = new LinkedList<Role>();
+		roles2.add(rol2);
+		
+		when(user1.getRoles()).thenReturn(roles1); //Asignamos los roles
+		when(user2.getRoles()).thenReturn(roles2);
+		user1.setRoles(roles2); //Cambiamos el rol del primer usuario
+		
 		@SuppressWarnings("deprecation")
-		Date date = new Date(1,1,1);
+		Date date1 = new Date(1,1,1);
 		@SuppressWarnings("deprecation")
 		Date date2 = new Date(2,2,2);
-		Collection<Role> roles;
-		Role rol = new Role("Rol");
-		roles = new LinkedList<Role>();
-		roles.add(rol);
-		user = new User("email", "userName", "password", "name", "surname", date,
-				"bio", roles, "", "");
-		User auxUser = user;
-		auxUser.setBio("bio2");
-		auxUser.setBirthdate(date2);
-		auxUser.setEmail("email2");
-		auxUser.setName("name2");
-		auxUser.setPassword("password2");
-		Collection<Role> rolesAux;
-		Role rolAux = new Role("Rol2");
-		rolesAux = new LinkedList<Role>();
-		rolesAux.add(rolAux);
-		auxUser.setRoles(rolesAux);
-		auxUser.setSurname("surname2");
-		auxUser.setUserName("userName2");
-		assertEquals(false, user.getEmail().compareTo(auxUser.getEmail()) != 0);
-		assertEquals(false, user.getUserName().compareTo(auxUser.getUserName()) != 0);
-		assertEquals(false, user.getPassword().compareTo(auxUser.getPassword()) != 0);
-		assertEquals(false, user.getName().compareTo(auxUser.getName()) != 0);
-		assertEquals(false, user.getSurname().compareTo(auxUser.getSurname()) != 0);
-		assertEquals(false, user.getBirthdate().compareTo(auxUser.getBirthdate()) != 0);
-		assertEquals(false, user.getBio().compareTo(auxUser.getBio()) != 0);
-		assertEquals(true, user.getRoles().containsAll(auxUser.getRoles()));
+		
+		when(user1.getBirthdate()).thenReturn(date1); //Asignamos las fechas
+		when(user2.getBirthdate()).thenReturn(date2);
+		user1.setBirthdate(date2); //Cambiamos la fecha de nacimiento del primer usuario
+		
+		String email1 = "1";
+		String email2 = "2";
+		
+		when(user1.getEmail()).thenReturn(email1); //Asignamos los emails
+		when(user2.getEmail()).thenReturn(email2);
+		user1.setEmail(email2); //Cambiamos el email del primer usuario
+		
+		String uName1 = "1";
+		String uName2 = "2";
+		
+		when(user1.getUserName()).thenReturn(uName1); //Asignamos los nombres de usuario
+		when(user2.getUserName()).thenReturn(uName2);
+		user1.setUserName(uName2); //Cambiamos el nombre de usuario del primer usuario
+		
+		String pass1 = "1";
+		String pass2 = "2";
+		
+		when(user1.getPassword()).thenReturn(pass1); //Asignamos la contrasenya
+		when(user2.getPassword()).thenReturn(pass2);
+		user1.setPassword(pass2); //Cambiamos la contrasenya del primer usuario
+		
+		String name1 = "1";
+		String name2 = "2";
+		
+		when(user1.getName()).thenReturn(name1); //Asignamos los nombres
+		when(user2.getName()).thenReturn(name2);
+		user1.setName(name2); //Cambiamos el nombre del primer usuario
+		
+		String sur1 = "1";
+		String sur2 = "2";
+		
+		when(user1.getSurname()).thenReturn(sur1); //Asignamos el apellido
+		when(user2.getSurname()).thenReturn(sur2);
+		user1.setSurname(sur2); //Cambiamos el apellido del primer usuario
+		
+		String bio1 = "1";
+		String bio2 = "2";
+		
+		when(user1.getBio()).thenReturn(bio1); //Asignamos la bio
+		when(user2.getBio()).thenReturn(bio2);
+		user1.setBio(bio2); //Cambiamos la bio del primer usuario
+		
+		String wL1 = "1";
+		String wL2 = "2";
+		when(user1.getWatchLater()).thenReturn(wL1); //Asignamos la lista de ver mas tarde
+		when(user2.getWatchLater()).thenReturn(wL2);
+		user1.setWatchLater(wL2); //Cambiamos la del primer usuario
+		
+		String mMovies1 = "1";
+		String mMovies2 = "2";
+		
+		when(user1.getMyMovies()).thenReturn(mMovies1); //Asignamos la lista de peliculas
+		when(user2.getMyMovies()).thenReturn(mMovies2);
+		user1.setMyMovies(mMovies2); //Cambiamos la del primer usuario
+		
+		assertEquals(false, user1.getId().equals(user2.getId())); //Comprueba el id
+		assertEquals(false, user1.getRoles().equals(user2.getRoles())); //Comprueba los roles
+		assertEquals(false, user1.getBirthdate().equals(user2.getBirthdate())); //Comprueba la fecha
+		assertEquals(false, user1.getEmail().equals(user2.getEmail())); //Comprueba el email
+		assertEquals(false, user1.getUserName().equals(user2.getUserName())); //Comprueba el nombre de usuario
+		assertEquals(false, user1.getPassword().equals(user2.getPassword())); //Comprueba la contrasenya
+		assertEquals(false, user1.getName().equals(user2.getName())); //Comprueba el nombre
+		assertEquals(false, user1.getSurname().equals(user2.getSurname())); //Comprueba el usuario
+		assertEquals(false, user1.getBio().equals(user2.getBio())); //Comprueba la bio
+		assertEquals(false, user1.getMyMovies().equals(user2.getMyMovies())); //Comprueba la lista de peliculas
+		assertEquals(false, user1.getWatchLater().equals(user2.getWatchLater())); //Comprueba la lista de ver mas tarde
+		
 	}
+
 	
 }
